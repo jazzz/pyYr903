4ee521739be0d15bb4be13608321100b6212cf21
Jazz Turner-Baggs <jazz.alyxzander@gmail.com>
2017-08-15 11:30:03 -0700

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,10 @@
+=================================
+ pyYr903
+=================================
+
+This module is used to communicate with the Yr903 RFID scanner.
+
+
+Installation
+============
+``python setup.py install` should work.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='yr903',
+    version='0.1',
+    packages=[''],
+    url='www.google.com',
+    license='',
+    author='Jazz Alyxzander Turner-Baggs',
+    author_email='jazz.alyxzander@gmail.com',
+    description='A python interface for the Yr903 UHF RFID reader'
+)
